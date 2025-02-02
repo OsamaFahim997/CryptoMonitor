a6c429d4939b1c9efcff1a09c500d94d9ff99878
Osama Fahim <osama.fahim@solera.com>
2025-02-02 17:20:10 +0100

    Initial Commit

---
+++ CryptoMonitor/CryptoMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitorApp.swift
+//  CryptoMonitor
+//
+//  Created by Osama Fahim on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
